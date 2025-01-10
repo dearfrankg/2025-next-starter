76b62691ed3f3f35ff5e6f9fb1953d8d4bd1f82a
Frank Gutierrez <dearfrankg@gmail.com>
2025-01-10 04:41:43 -0800

    add eslint plugins and lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,43 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat();
 
 const eslintConfig = [
+  {
+    ignores: [".next/", "node_modules/"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.mjs"],
+
+    languageOptions: {
+      parser: typescriptEslintParser,
+      parserOptions: {
+        project: "./tsconfig.json", // Ensure TypeScript is aware of the project configuration
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      import: eslintPluginImport,
+      tailwindcss: eslintPluginTailwindcss,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "tailwindcss/classnames-order": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
